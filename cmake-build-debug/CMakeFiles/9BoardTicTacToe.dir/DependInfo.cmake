09697131fcb87324ee1eb24c86ba823a1736cfdd
Nizar Abak-kali <nizarabakkali93@gmail.com>
2017-01-04 19:33:30 +0100

    push sa joue

--- cmake-build-debug/CMakeFiles/9BoardTicTacToe.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/9BoardTicTacToe.dir/DependInfo.cmake
@@ -4,11 +4,11 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/nizar/ClionProjects/9BoardTicTacToe/Board.cpp" "/home/nizar/ClionProjects/9BoardTicTacToe/cmake-build-debug/CMakeFiles/9BoardTicTacToe.dir/Board.cpp.o"
-  "/home/nizar/ClionProjects/9BoardTicTacToe/Coup.cpp" "/home/nizar/ClionProjects/9BoardTicTacToe/cmake-build-debug/CMakeFiles/9BoardTicTacToe.dir/Coup.cpp.o"
-  "/home/nizar/ClionProjects/9BoardTicTacToe/Player.cpp" "/home/nizar/ClionProjects/9BoardTicTacToe/cmake-build-debug/CMakeFiles/9BoardTicTacToe.dir/Player.cpp.o"
-  "/home/nizar/ClionProjects/9BoardTicTacToe/ia.cpp" "/home/nizar/ClionProjects/9BoardTicTacToe/cmake-build-debug/CMakeFiles/9BoardTicTacToe.dir/ia.cpp.o"
-  "/home/nizar/ClionProjects/9BoardTicTacToe/main.cpp" "/home/nizar/ClionProjects/9BoardTicTacToe/cmake-build-debug/CMakeFiles/9BoardTicTacToe.dir/main.cpp.o"
+  "/home/nizar/CLionProjects/NineBoardTicTacToe/Board.cpp" "/home/nizar/CLionProjects/NineBoardTicTacToe/cmake-build-debug/CMakeFiles/9BoardTicTacToe.dir/Board.cpp.o"
+  "/home/nizar/CLionProjects/NineBoardTicTacToe/Coup.cpp" "/home/nizar/CLionProjects/NineBoardTicTacToe/cmake-build-debug/CMakeFiles/9BoardTicTacToe.dir/Coup.cpp.o"
+  "/home/nizar/CLionProjects/NineBoardTicTacToe/Player.cpp" "/home/nizar/CLionProjects/NineBoardTicTacToe/cmake-build-debug/CMakeFiles/9BoardTicTacToe.dir/Player.cpp.o"
+  "/home/nizar/CLionProjects/NineBoardTicTacToe/ia.cpp" "/home/nizar/CLionProjects/NineBoardTicTacToe/cmake-build-debug/CMakeFiles/9BoardTicTacToe.dir/ia.cpp.o"
+  "/home/nizar/CLionProjects/NineBoardTicTacToe/main.cpp" "/home/nizar/CLionProjects/NineBoardTicTacToe/cmake-build-debug/CMakeFiles/9BoardTicTacToe.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
